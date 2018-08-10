f98b7db944222f61e7f1b950cbe537ab0e0b7ead
Jane Costa <yberdysh@gmail.com>
2018-08-10 12:01:01 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,50 @@
+let store = {drivers: [], passengers: [], trips: []}
+
+
+let driverId = 0;
+let passengerId = 0
+let trips = 0
+
+class Driver{
+	constructor(name){
+		this.name = name
+		this.id = ++driverId
+		store.drivers.push(this)
+	}
+	trips(){
+		return store.trips.filter(trip => trip.driverId === this.id)
+	}
+	passengers(){
+		return this.trips().map(trip => trip.passenger())
+	}
+}
+
+class Passenger{
+	constructor(name){
+		this.name = name
+		this.id = ++passengerId
+		store.passengers.push(this)
+	}
+
+	trips(){
+		return store.trips.filter(trip => trip.passengerId === this.id)
+	}
+	drivers(){
+		return this.trips().map(trip => trip.driver())
+	}
+}
+
+class Trip{
+	constructor(driver, pass){
+		this.driverId = driver.id
+		this.passengerId = pass.id
+		this.id = ++trips
+		store.trips.push(this)
+	}
+	driver(){
+		return store.drivers.find(driver => driver.id === this.driverId)
+	}
+	passenger(){
+		return store.passengers.find(pass => pass.id === this.passengerId)
+	}
+}
